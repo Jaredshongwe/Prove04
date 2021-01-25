0a319127c41d3dac0bd4bc8a14be50e73ad574d3
Jaredshongwe <jaredshongwe@gmail.com>
2021-01-25 21:08:46 +0200

    removing from HelloFromJared.java

--- src/shongwe/HelloFromJared.java
+++ src/shongwe/HelloFromJared.java
@@ -4,6 +4,5 @@ public class HelloFromJared {
 
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Jared");
-	System.out.println("And I am the greatest");
     }
 }
