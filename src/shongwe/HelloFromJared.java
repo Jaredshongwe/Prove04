d640440440eb5e2feef832b3e0d394a5cc9df1fd
Zak <sha19018@byui.edu>
2021-01-27 20:59:04 +0200

    Add Zak's response

--- src/shongwe/HelloFromJared.java
+++ src/shongwe/HelloFromJared.java
@@ -3,6 +3,7 @@ package shongwe;
 public class HelloFromJared {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Jared");
+	    System.out.println("Hello, my name is Jared");
+        System.out.println("Hello Jared, my name is Zak.");
     }
 }
